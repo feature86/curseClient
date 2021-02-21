c82851b9448b93bc516749ef32434cf43d7e909c
Paul Picher <paul@picher.cc>
2021-02-21 20:18:30 +0100

    add Container, Components, styled-components

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+interface AvatarImageProps {
+    url: String; 
+}
+
+interface AvatarProps extends AvatarImageProps {
+    name: String;
+}
+
+const AvatarImage = styled.div<AvatarImageProps>`
+    height: 70%;
+    width: 70%;
+    background-size: cover;
+    background: url( ${ ({ url }) => `${url}`});
+    border: 1px solid red;
+`
+const AvatarWrap = styled.div`
+  width: 200px;
+  height: 200px;
+  border: 1px solid green;
+  margin: 10px;
+  display: flex;
+  flex: 0 0 auto;
+  flex-direction: column; 
+  align-items:center;
+  justify-content: space-around;
+
+`;
+
+const AvatarName = styled.div`
+    font-size: 16px;
+    color: black;
+    
+`;
+
+
+export const Avatar: React.FC<AvatarProps> = ({ url, name}) => {
+    return (
+        <AvatarWrap>
+            <AvatarImage url={ url}/> 
+            <AvatarName>{ name}</AvatarName>
+        </AvatarWrap>
+    )
+}
---
+++ src/container/App/index.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Avatar} from '../../components/Avatar';
+
+
+const AppContainer = styled.div`
+ width: 100vw;
+ height: 100vh;
+ background-color: burlywood;
+ display: flex;
+ flex: 0 0 auto;
+ flex-direction: column; 
+ justify-content: center;
+ align-items: center;
+`;
+
+const ContainerWrap = styled.div`
+    width: 50%;
+    height: 50%;
+    display: flex;
+    flex: 0 0 auto; 
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+`
+const Heading = styled.div`
+    font-size: 22px;
+    color: lightgoldenrodyellow;
+    
+`;
+
+const App: React.FC = () => {
+
+    const users = [{
+        name: 'user1',
+        url: 'asd',
+        userHash: '7b6405c4-7477-11eb-9439-0242ac130002'
+    }, {
+        name: 'user2',
+        url: 'asd',
+        userHash: '86e133d6-7477-11eb-9439-0242ac130002',
+    }]
+
+    
+
+    return (
+        <AppContainer>
+            <Heading>
+                Fluch-Counter
+        </Heading>
+        <ContainerWrap>
+                {
+                    users.map((u) => {
+                        return <Avatar name={u.name} url={u.url} />
+                    })
+                }
+        </ContainerWrap>
+    </AppContainer>
+    )
+}
+
+export default App;
